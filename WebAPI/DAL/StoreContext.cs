31390408b4ccbf221330a4c7eb1029fbea146085
fifarafka <wojtasikmonikia@gmail.com>
2016-04-25 23:43:35 +0200

    zadania

---
+++ WebAPI/DAL/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebApp.Models;
+
+namespace WebApp.DAL
+{
+    public class StoreContext : DbContext
+    {
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ WebAPI/DAL/StoreInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebApp.Models;
+
+namespace WebApp.DAL
+{
+    public class StoreInitializer : DropCreateDatabaseIfModelChanges<StoreContext>
+    {
+        protected override void Seed(StoreContext context)
+        {
+            var authors = new List<Author>
+            {
+                new Author() {AuthorName = "Adrianna", AuthorSurname = "Grande"},
+                new Author() {AuthorName = "Janusz", AuthorSurname = "Bieber"},
+
+            };
+            authors.ForEach(i => context.Authors.Add(i));
+            context.SaveChanges();
+
+            var books = new List<Book>()
+            {
+                new Book() {BookTitle = "Dlaczego ja?"},
+                new Book() {BookTitle="Pamietniki z wakacji"}
+            };
+            books.ForEach(g => context.Books.Add(g));
+            context.SaveChanges();
+
+        }
+    }
+}
